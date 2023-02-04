c8ed2dd338d2574df88d9fbb7d080a64d98c6320
Rohan Patil <rohanpatil@Rohans-MacBook-Air.local>
2023-02-04 11:17:58 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+
+
+
+app = FastAPI()
+
+
+class ScoreItem(BaseModel):
+    Pregnancies:int
+    Glucose: int
+    BloodPressure:int
+    SkinThickness:int
+    Insulin:int
+    BMI:float
+    DiabetesPedigreeFunction:float
+    Age:int
+
+with open("Log_model.pkl","rb") as f:
+    model = pickle.load(f)
+
+
+
+@app.post("/predict")
+async def getData(item:ScoreItem):
+    data = pd.DataFrame([item.dict().values()],columns= item.dict().keys())
+    y = model.predict(data)
+
+    return {'prediction':int(y)}
